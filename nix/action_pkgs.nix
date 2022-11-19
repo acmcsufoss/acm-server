9f90b4100d64bae1841969f0620fe81992a8ce90
diamondburned <diamond@arikawa-hi.me>
2022-11-19 03:18:41 -0800

    Add deploy workflow

--- nix/action_pkgs.nix
+++ nix/action_pkgs.nix
@@ -5,6 +5,4 @@ let commit = "ac20a8605b0f79be2d65d995cd347251cd5b984b"; # nixos-22.05
 	};
 	pkgs = import pkgsSrc {};
 
-in import ./dev_pkgs.nix {
-	inherit pkgs;
-}
+in import ./dev_pkgs.nix pkgs
