5d7158cdfab7ce6dd738b3415d93beab91b5b092
diamondburned <diamond@arikawa-hi.me>
2022-11-19 23:02:50 -0800

    Edit Caddy modSha256

--- servers/cirno/configuration.nix
+++ servers/cirno/configuration.nix
@@ -17,7 +17,7 @@
 			# This fixes some quirky Go module conflict.
 			"github.com/antlr/antlr4/runtime/Go/antlr v0.0.0-20220418222510-f25a4f6275ed"
 		];
-		modSha256 = "16vqag9pqhcrbbk8s9j4bx6pzbpnic0acpfk7wzwk9q47izxk0wa";
+		modSha256 = "sha256-ioPZfzwEp8k/P9NdpgCL9q5/TV9EJo3mWplBfNNTeJs=";
 	};
 
 	systemd.services.acmregister = {
