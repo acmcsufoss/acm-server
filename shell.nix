06133adbb588fae44ebd6cdbaf89561d09497fe4
diamondburned <diamond@arikawa-hi.me>
2022-11-24 21:10:36 -0800

    Remove redundant nix/shell-pkgs.nix

--- shell.nix
+++ shell.nix
@@ -2,5 +2,16 @@
 
 pkgs.mkShell {
 	name = "acm-aws-shell";
-	buildInputs = import ./nix/shell-pkgs.nix;
+	buildInputs = with pkgs; [
+		terraform
+		awscli2
+		nix_2_3
+		nix-update
+		niv
+		git
+		git-crypt
+		yamllint
+		gomod2nix
+		expect
+	];
 }
