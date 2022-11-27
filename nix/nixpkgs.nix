dbc3b73f47eb9e1a3a7d273016823d84e46d7821
diamondburned <diamond@arikawa-hi.me>
2022-11-27 02:36:44 -0800

    Move Nixpkgs overlays to a separate file

--- nix/nixpkgs.nix
+++ nix/nixpkgs.nix
@@ -1,12 +1,5 @@
 let sources = import ./sources.nix;
 
 in import sources.nixpkgs {
-	overlays = [
-		(import "${sources.gomod2nix}/overlay.nix")
-		(self: super: {
-			nix-update = super.nix-update.overrideAttrs (old: {
-				src = sources.diamondburned_nix-update;
-			});
-		})
-	];
+	overlays = import ./overlays.nix;
 }
---
+++ nix/overlays.nix
@@ -0,0 +1,17 @@
+let sources = import ./sources.nix;
+	nix-npm-buildpackage = self: super:
+		let pkg = super.callPackage sources.nix-npm-buildpackage { };
+		in {
+			inherit (pkg) buildNpmPackage buildYarnPackage;
+		};
+
+in [
+	(import "${sources.gomod2nix}/overlay.nix")
+	(nix-npm-buildpackage)
+	(self: super: {
+		nix-update = super.nix-update.overrideAttrs (old: {
+			src = sources.diamondburned_nix-update;
+		});
+	})
+	(self: super: import ../packages { pkgs = self; })
+]
