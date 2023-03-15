9b086b9a1298518994d010219d82d2cd281c8d70
EthanThatOneKid <EthanThatOneKid@users.noreply.github.com>
2023-03-15 22:27:18 +0000

    Update package pomo using GitHub Actions
    Package pomo was updated to version:
    
        latest
    
    The action workflow was triggered by ref:
    
        b82488c593319fa9bf6d23198712e1a1831aa4a9

--- packages/pomo/default.nix
+++ packages/pomo/default.nix
@@ -4,5 +4,5 @@ buildDenoPackage rec {
 	pname = "pomo";
 	src = (import <acm-aws/nix/sources.nix>).pomo;
 	entrypoint = "server/main.ts";
-	outputHash = "sha256:0bi1hcgd36073h834xa5xwsmz2j3zailknlal7nn6lqnywgf3cz4";
+	outputHash = "sha256-0Xq//RHtJYG5u8qBLmuVh8wzQtpq0r5UOvPQdH7Qzy0=";
 }
