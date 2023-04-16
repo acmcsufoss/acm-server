e2d2f0497550a4a68f2753b05dbbaa30717e9e45
diamondburned <diamond@arikawa-hi.me>
2023-04-15 19:32:38 -0700

    Bump acmregister manually

--- packages/acmregister/default.nix
+++ packages/acmregister/default.nix
@@ -5,7 +5,7 @@ buildGo119Module rec {
 	version = builtins.substring 0 7 src.rev;
 
 	src = (import <acm-aws/nix/sources.nix>).acmregister;
-	vendorSha256 = "sha256-hWummvDn2sfg/7yj35+qeGSJb31DZmIA4NTU35CLF3I=";
+	vendorSha256 = "sha256:0mh4awgj91zz81yqvlf12fpvfgsvk7h758vygkfhccdg42giic6h";
 
 	# GOWORK is incompatible with vendorSha256.
 	GOWORK = "off";
