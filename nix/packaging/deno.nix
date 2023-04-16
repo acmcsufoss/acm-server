3024a04b33d94c4a2cdd3943f534fec4c3c54905
diamondburned <diamond@arikawa-hi.me>
2023-04-15 21:01:55 -0700

    Make Deno builds reproducible
    DENO_DIR is weird. They actually throw the current timestamp in DENO_DIR
    for no particular reason. This ensures that builds are NEVER
    reproducible! What a wonderful idea.
    
    This commit creates a deno-cache derivation similar to go-modules and
    filters the .metadata.json file so that it's reproducible. This should
    fix all the build inconsistencies.

--- nix/packaging/deno.nix
+++ nix/packaging/deno.nix
@@ -1,4 +1,4 @@
-{ runCommand, deno, makeWrapper, writeShellScript }:
+{ runCommand, deno, makeWrapper, writeShellScript, jq, tree }:
 
 let pkgutil = import <acm-aws/nix/pkgutil.nix>;
 in
@@ -8,32 +8,60 @@ in
 	pname,
 	version ? pkgutil.version src,
 	src,
+	lockfile ? "",
 	entrypoint,
 	outputHash,
+	buildInputs ? [],
 	...
 }@args:
 
-let denoPkg = runCommand "${name}-deno-pkg" (args // {
-		inherit (args) src entrypoint outputHash;
+let # https://deno.land/manual@v1.32.4/basics/modules/integrity_checking
+	denoDir = runCommand "${name}-deno-cache" {
+		inherit src entrypoint lockfile outputHash;
 		outputHashMode = "recursive";
-		nativeBuildInputs = [ deno ];
-	}) ''
+		nativeBuildInputs = [ deno jq ];
+	} ''
+		mkdir .deno
 		export DENO_DIR=.deno
-		deno compile -A --output ${pname} "$src/$entrypoint"
-		mkdir -p $out/bin
-		install -Dm755 ${pname} $out/bin/${pname}
+
+		mkdir $out
+
+		{
+			if [[ "$lockfile" ]]; then
+				deno cache --reload --lock=$src/$lockfile "$src/$entrypoint"
+				cp $src/$lockfile $out/deno.lock
+			else
+				deno cache --reload --lock=deno.lock --lock-write "$src/$entrypoint" 
+				mv deno.lock $out/deno.lock
+			fi
+		} |& cat # deno is dumb
+
+		cp -r .deno/deps $out
+		for metadata in $(find $out/deps -name "*.metadata.json"); do
+			# deno is dumb part 2
+			jq '{ url, headers: {} }' "$metadata" > "$metadata.tmp"
+			mv "$metadata.tmp" "$metadata"
+		done
 	'';
 in
 
 runCommand name {
-	nativeBuildInputs = [ makeWrapper ];
-	buildInputs = [ denoPkg ];
+	inherit src entrypoint lockfile buildInputs;
+	nativeBuildInputs = [ makeWrapper deno denoDir ];
 	passthru = {
 		inherit outputHash;
 		outputHashMode = "recursive";
 	};
 } ''
-	mkdir -p $out/bin
-	makeWrapper ${denoPkg}/bin/${pname} $out/bin/${pname} \
+	mkdir .deno
+	cp -r ${denoDir}/deps .deno
+	export DENO_DIR=.deno
+
+	deno compile \
+		--lock=${denoDir}/deno.lock \
+		--output main.out -A "$src/$entrypoint"
+
+	install -Dm755 main.out $out/bin/${pname}
+	wrapProgram $out/bin/${pname} \
 		--set-default DENO_DIR '/tmp/.deno'
 ''
--- packages/pomo/default.nix
+++ packages/pomo/default.nix
@@ -4,5 +4,5 @@ buildDenoPackage rec {
 	pname = "pomo";
 	src = (import <acm-aws/nix/sources.nix>).pomo;
 	entrypoint = "server/main.ts";
-	outputHash = "sha256:0gzjvba7iz96798mzzmzgikkvg9kjh5ik6vf96z4wn8if49wkiyr";
+	outputHash = "sha256:094znhgg9bdlmr5hpsrkbhrl12lkf2mvn7ws8riy2n7z04dx1dl5";
 }
