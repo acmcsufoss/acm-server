9f53655fe877d68aa1b27290394627bac42ae6e7
diamondburned <diamond@arikawa-hi.me>
2023-10-05 15:14:08 -0700

    Migrate Quizler to cs306

--- servers/cs306/caddy/default.nix
+++ servers/cs306/caddy/default.nix
@@ -1,9 +1,20 @@
 { config, lib, pkgs, ... }:
 
+let
+	preprocessedCaddyfile = pkgs.runCommandLocal "Caddyfile-preprocessed" {} ''
+		cp ${./Caddyfile} $out
+
+		# Ensure that all acmcsuf.com domains are prefixed with "http://".
+		# Otherwise, Caddy will refuse to listen on port 80 at all, which
+		# we want for our reverse proxy in cirno.
+		sed -i 's/\(^\|, \)\([a-zA-Z0-9]*\.acmcsuf.com\)/\1http:\/\/\2/g' $out
+	'';
+in
+
 {
 	services.diamondburned.caddy = {
 		enable = true;
-		configFile = ./Caddyfile;
+		configFile = preprocessedCaddyfile;
 		environment = import <acm-aws/secrets/caddy-env.nix>;
 	};
 }
--- servers/cs306/services.nix
+++ servers/cs306/services.nix
@@ -3,5 +3,31 @@
 {
 	imports = [
 		./caddy
-	];
+		];
+	
+	systemd.services.quizler = {
+		enable = true;
+		description = "https://github.com/jacobtread/Quizler";
+		after = [ "network-online.target" ];
+		wantedBy = [ "multi-user.target" ];
+		environment = {
+			QUIZLER_PORT = "37867"; # see Caddyfile
+		};
+		serviceConfig = {
+			Type = "simple";
+			ExecStart = "${pkgs.quizler}/bin/quizler";
+			DynamicUser = true;
+			Restart = "on-failure";
+			RestartSec = "1s";
+			# Use strict resource controls since this is an exposed and untrusted
+			# service.
+			ProtectSystem = "strict";
+			ProtectHome = true;
+			PrivateDevices = true;
+			MemoryMax = "256M";
+			TasksMax = 128;
+			CPUQuota = "50%";
+			RestrictNetworkInterfaces = "lo"; # enough for localhost
+		};
+	};
 }
