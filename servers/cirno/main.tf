d5209f6ae08b61299f5ecd8ad6ec452f8a18c46d
diamondburned <diamond@arikawa-hi.me>
2023-10-06 08:06:12 -0700

    Expose Tailscale P2P for Cirno

--- servers/cirno/main.tf
+++ servers/cirno/main.tf
@@ -38,6 +38,13 @@ resource "aws_security_group" "cirno" {
 		protocol = "udp"
 		cidr_blocks = [ "0.0.0.0/0" ]
 	}
+	# Allow Tailscale ports.
+	ingress {
+		from_port = 41641
+		to_port = 41641
+		protocol = "udp"
+		cidr_blocks = [ "0.0.0.0/0" ]
+	}
 	egress {
 		from_port = 0
 		to_port	= 0
