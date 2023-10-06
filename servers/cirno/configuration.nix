544a38d5d0dd328784a3cefc0c5ce18552826e83
diamondburned <diamond@arikawa-hi.me>
2023-10-06 04:11:07 -0700

    Migrate more services to cs306, fixed pomo endpoint

--- servers/cirno/configuration.nix
+++ servers/cirno/configuration.nix
@@ -21,11 +21,6 @@ in
 		environment = import <acm-aws/secrets/caddy-env.nix>;
 	};
 
-	services.dischord = {
-		enable = true;
-		config = builtins.readFile ./secrets/dischord-config.toml;
-	};
-
 	systemd.services.acmregister = {
 		enable = true;
 		description = "ACM member registration Discord bot";
@@ -37,21 +32,6 @@ in
 			ExecStart = "${pkgs.acmregister}/bin/acmregister";
 			Restart = "on-failure";
 			RestartSec = "1s";
-		};
-	};
-
-	systemd.services.acm-nixie = {
-		enable = true;
-		description = "acmCSUF's version of the nixie bot.";
-		after = [ "network-online.target" ];
-		wantedBy = [ "multi-user.target" ];
-		environment = import ./secrets/acm-nixie-env.nix;
-		serviceConfig = {
-			Type = "simple";
-			ExecStart = "${pkgs.acm-nixie}/bin/acm-nixie";
-			DynamicUser = true;
-			StateDirectory = "acm-nixie";
-			ReadWritePaths = [ "/var/lib/acm-nixie" ];
 		};
 	};
 
@@ -77,23 +57,6 @@ in
 				RestartSec = "1s";
 			};
 		};
-
-	systemd.services."goworkshop.acmcsuf.com" =
-		let
-			shell = import "${sources.go-workshop}/shell.nix" { inherit pkgs; };
-			present = shell.present;
-		in
-			{
-				enable = true;
-				description = "Go x/tools/present server for goworkshop.acmcsuf.com";
-				after = [ "network.target" ];
-				wantedBy = [ "multi-user.target" ];
-				serviceConfig = {
-					ExecStart = "${present}/bin/present --play=true --use_playground --http=127.0.0.1:38572";
-					DynamicUser = true;
-					WorkingDirectory = "${sources.go-workshop}";
-				};
-			};
 
 	systemd.services.discord-ical-reminder =
 		let
--- servers/cs306/services.nix
+++ servers/cs306/services.nix
@@ -1,5 +1,9 @@
 { config, lib, pkgs, ... }:
 
+let
+	sources = import <acm-aws/nix/sources.nix>;
+in
+
 {
 	imports = [
 		./caddy
@@ -60,4 +64,41 @@
 			RestartSec = "1s";
 		};
 	};
+	
+	systemd.services.acm-nixie = {
+		enable = true;
+		description = "acmCSUF's version of the nixie bot.";
+		after = [ "network-online.target" ];
+		wantedBy = [ "multi-user.target" ];
+		environment = import <acm-aws/secrets/acm-nixie-env.nix>;
+		serviceConfig = {
+			Type = "simple";
+			ExecStart = "${pkgs.acm-nixie}/bin/acm-nixie";
+			DynamicUser = true;
+			StateDirectory = "acm-nixie";
+			ReadWritePaths = [ "/var/lib/acm-nixie" ];
+		};
+	};
+
+	services.dischord = {
+		enable = true;
+		config = builtins.readFile <acm-aws/secrets/dischord-config.toml>;
+	};
+
+	systemd.services."goworkshop.acmcsuf.com" =
+		let
+			shell = import "${sources.go-workshop}/shell.nix" { inherit pkgs; };
+			present = shell.present;
+		in
+			{
+				enable = true;
+				description = "Go x/tools/present server for goworkshop.acmcsuf.com";
+				after = [ "network.target" ];
+				wantedBy = [ "multi-user.target" ];
+				serviceConfig = {
+					ExecStart = "${present}/bin/present --play=true --use_playground --http=127.0.0.1:38572";
+					DynamicUser = true;
+					WorkingDirectory = "${sources.go-workshop}";
+				};
+			};
 }
