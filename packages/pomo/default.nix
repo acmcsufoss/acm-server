57b9fcb5da460886b41954481438f4c54679b261
diamondburned <diamond@arikawa-hi.me>
2023-12-01 06:04:54 -0800

    Update pomo outputHash
    Hopefully this isn't another symptom of Deno being trash...

--- packages/pomo/default.nix
+++ packages/pomo/default.nix
@@ -4,5 +4,5 @@ buildDenoPackage rec {
 	pname = "pomo";
 	src = (import <acm-aws/nix/sources.nix>).pomo;
 	entrypoint = "server/main.ts";
-	outputHash = "sha256:094znhgg9bdlmr5hpsrkbhrl12lkf2mvn7ws8riy2n7z04dx1dl5";
+	outputHash = "sha256-+nz8sQWrz7Rl6eMYdUbRPSP158qL0uFXA1JV5Iy0wx4";
 }
