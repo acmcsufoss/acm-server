ca6c27504d91f17d9527beeea6f305ed1ab92a2d
diamondburned <diamond@arikawa-hi.me>
2024-02-09 04:52:19 -0800

    Fix mkForce missing parentheses

--- containers/cs306/test.nix
+++ containers/cs306/test.nix
@@ -76,6 +76,6 @@ in
 	systemd.services.${podmanServiceName "experimental-discord-bot"} = with lib; {
 		serviceConfig.RestartSec = mkForce "5s";
 		startLimitBurst = mkForce 3;
-		startLimitIntervalSec = mkForce 5 * 60; # 5 minutes
+		startLimitIntervalSec = mkForce (5 * 60); # 5 minutes
 	};
 }
