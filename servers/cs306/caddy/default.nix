b7eb43d0bba39d96e4e4c9eab069c74f6180c43c
diamondburned <diamond@arikawa-hi.me>
2024-02-22 02:06:48 -0800

    Fix ip_sources.static not doing variable expansion

--- servers/cs306/caddy/default.nix
+++ servers/cs306/caddy/default.nix
@@ -1,6 +1,8 @@
 { config, lib, pkgs, ... }:
 
 let
+	environment = import <acm-aws/secrets/caddy-env.nix>;
+
 	preprocessedCaddyfile = pkgs.runCommandLocal "Caddyfile-preprocessed" {} ''
 		cp ${./Caddyfile} $out
 
@@ -8,6 +10,9 @@ let
 		# Otherwise, Caddy will refuse to listen on port 80 at all, which
 		# we want for our reverse proxy in cirno.
 		sed -i 's/\(^\|, \)\([a-zA-Z0-9\-_.]*\.acmcsuf.com\)/\1http:\/\/\2/g' $out
+
+		# ip_sources.static doesn't do expansion, so we have to do it manually.
+		sed -i 's/{env.CADDY_PUBLIC_IP}/${environment.CADDY_PUBLIC_IP}/g' $out
 	'';
 in
 
@@ -15,6 +20,6 @@ in
 	services.diamondburned.caddy = {
 		enable = true;
 		configFile = preprocessedCaddyfile;
-		environment = import <acm-aws/secrets/caddy-env.nix>;
+		inherit environment;
 	};
 }
