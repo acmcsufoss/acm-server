cc3aae94acd4509ccb1290a46c195844edec617d
diamondburned <diamond@libdb.so>
2024-03-11 19:04:38 -0700

    Disable fullyhacks-qrms; move some services to CS306

--- servers/cs306/services.nix
+++ servers/cs306/services.nix
@@ -95,6 +95,38 @@ in
 		};
 	};
 
+	services.christmasd-test = {
+		enable = true;
+		ledPointsFile = builtins.fetchurl
+			"https://gist.githubusercontent.com/diamondburned/1d9a83347e153686ca192c6f5baf0b79/raw/63b06ed82fd858bdb8b15a5971df92dd4bab40c3/led-points.csv";
+		extraFlags = {
+			http-addr = "unix://$RUNTIME_DIRECTORY/http.sock";
+		};
+	};
+
+	systemd.services.sendlimiter =
+		let
+			extraArgs = [];
+			secrets = import <acm-aws/secrets/sendlimiter.nix>;
+			args = lib.concatStringsSep
+				" "
+				(map lib.escapeShellArg (extraArgs ++ secrets.channelIDs));
+		in {
+			enable = true;
+			description = "Send limiter Discord bot";
+			after = [ "network-online.target" ];
+			wantedBy = [ "multi-user.target" ];
+			environment = {
+				BOT_TOKEN = secrets.botToken;
+			};
+			serviceConfig = {
+				Type = "simple";
+				ExecStart = "${pkgs.sendlimiter}/bin/sendlimiter ${args}";
+				Restart = "on-failure";
+				RestartSec = "1s";
+			};
+		};
+
 	services.dischord = {
 		enable = true;
 		config = builtins.readFile <acm-aws/secrets/dischord-config.toml>;
