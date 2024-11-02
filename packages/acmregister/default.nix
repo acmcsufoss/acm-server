bb529741ff708b7305b94759bb308e1d3318193e
diamondburned <diamond@libdb.so>
2024-11-01 22:53:14 -0700

    Update acmregister

--- packages/acmregister/default.nix
+++ packages/acmregister/default.nix
@@ -1,13 +1,15 @@
-{ buildGo119Module, fetchFromGitHub, lib }:
+{
+  buildGo119Module,
+  fetchFromGitHub,
+  lib,
+}:
 
-buildGo119Module rec {
-	pname = "acmregister";
-	version = builtins.substring 0 7 src.rev;
+let
+  sources = import <acm-aws/nix/sources.nix>;
+  acmregister =
+    (import sources.flake-compat {
+      src = sources.acmregister;
+    }).defaultNix;
+in
 
-	src = (import <acm-aws/nix/sources.nix>).acmregister;
-	vendorSha256 = "sha256-QpAj9IGcLEJdPBghrK2nmbk/sZmtY6hB3w7dw2+l7zQ=";
-
-	# GOWORK is incompatible with vendorSha256.
-	GOWORK = "off";
-	subPackages = [ "." ];
-}
+acmregister
