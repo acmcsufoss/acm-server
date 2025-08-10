8840149d5f761476ec3a88fa7d6a1d41c1e9695e
diamondburned <diamond@libdb.so>
2025-08-10 16:27:56 -0700

    Reopen SSH ingress to AWS
    This was sealed off when there was an OpenSSH vulnerability, but
    it should be safe to reopen now.

--- servers/cirno/main.tf
+++ servers/cirno/main.tf
@@ -24,12 +24,12 @@ locals {
 }
 
 resource "aws_security_group" "cirno" {
-	# ingress {
-	# 	from_port = 22
-	# 	to_port = 22
-	# 	protocol = "tcp"
-	# 	cidr_blocks = [ "0.0.0.0/0" ]
-	# }
+	ingress {
+		from_port = 22
+		to_port = 22
+		protocol = "tcp"
+		cidr_blocks = [ "0.0.0.0/0" ]
+	}
 	ingress {
 		from_port = 80
 		to_port = 80
